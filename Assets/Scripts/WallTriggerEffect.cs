a5bb5653fdeaf43e2fb9fda32c5c4d2e15e6d9a0
oleg <oleg.brovchenkov@gmail.com>
2024-03-12 19:39:20 +0100

    Wall threat effects improved

--- Assets/Scripts/WallTriggerEffect.cs
+++ Assets/Scripts/WallTriggerEffect.cs
@@ -7,6 +7,7 @@ public class WallTriggerEffect : MonoBehaviour
     
     [Tooltip("Mask describes how player's position affects walls trigger effect intensity. Vertical or horizontal primarily")]
     public Vector2 mask;
+    public float factor = 1.0f;
 
     private void Start()
     {
@@ -17,7 +18,7 @@ public class WallTriggerEffect : MonoBehaviour
     {
         float v = Vector2.Distance((playerPosition * mask), transform.position * mask) / 2f;
         float a = 1 - Mathf.Clamp01(Mathf.Pow(v, 2));
-        image.color = new Color(image.color.r, image.color.g, image.color.b, a);
+        image.color = new Color(image.color.r, image.color.g, image.color.b, a * factor);
     }
 
     private void FixedUpdate()
