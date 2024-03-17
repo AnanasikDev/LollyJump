7be907387e4685a5795d60cc91031c92e5b70e24
oleg <oleg.brovchenkov@gmail.com>
2024-03-17 14:20:11 +0100

    Little effect added (en.wikipedia.org/wiki/Why_Don%27t_You%3F)

--- Assets/Scripts/IdleFlashEffect.cs
+++ Assets/Scripts/IdleFlashEffect.cs
@@ -1,14 +1,23 @@
+using TMPro;
 using UnityEngine;
 
 public class IdleFlashEffect : MonoBehaviour
 {
     private Animator animator;
     private static float timeToActivateSeconds = 5;
+    private static float timeToActivateEastereggSeconds = 73; // 73
+    private static float timeToDisactivateEastereggSeconds = 95; // 95
     private bool active = false;
+
+    [SerializeField] private TextMeshProUGUI text;
+
+    private string baseEffect = "Tap anywhere\nto start";
+    private string eastereggEffect = "Why don't you?";
 
     private void Start()
     {
         animator = GetComponent<Animator>();
+        text.text = baseEffect;
     }
 
     private void Update()
@@ -17,6 +26,15 @@ public class IdleFlashEffect : MonoBehaviour
         {
             if (!active)
                 BeginEffect();
+            else if (Environment.inputController.timeSinceLastAction >= timeToActivateEastereggSeconds &&
+                     Environment.inputController.timeSinceLastAction < timeToDisactivateEastereggSeconds)
+            {
+                text.text = eastereggEffect;
+            }
+            else if (Environment.inputController.timeSinceLastAction >= timeToDisactivateEastereggSeconds)
+            {
+                text.text = baseEffect;
+            }
         }
         else if (active)
         {
